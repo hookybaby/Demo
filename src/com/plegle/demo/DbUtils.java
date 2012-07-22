845615ac92459115e1c0de7fd1172f6489d47381
hookybaby <hookybaby@gmail.com>
2012-07-22 18:20:36 +0800

    delete DbUtils.java content

--- src/com/plegle/demo/DbUtils.java
+++ src/com/plegle/demo/DbUtils.java
@@ -1,23 +1,5 @@
 package com.plegle.demo;
 
 public class DbUtils {
-	private String userName;
-	private String password;
 
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		System.out.println("bingo");
-		this.password = password;
-	}
 }
